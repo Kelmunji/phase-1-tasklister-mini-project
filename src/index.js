e6d22f88ff459485d65be731866c470182233507
kelvin Githinji <githinjikelvin74@gmail.com>
2025-03-17 09:31:14 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ document.addEventListener("DOMContentLoaded",() => {
 
     const li = document.createElement('li');li.textContent =taskDescription;
 
+    const deleteButton = document.createElement('button');
+          deleteButton.textContent = 'Delete';
+          deleteButton.className = 'delete-task';
+
+          deleteButton.addEventListener('click', function(){
+            li.remove();
+          });
+
+    li.appendChild(deleteButton);      
+
     document.getElementById('tasks').appendChild(li);
 
     document.getElementById('new-task-description').value ='';
